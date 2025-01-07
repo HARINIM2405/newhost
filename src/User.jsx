f6bde749d20d9b59feba16aa7b26c1a84f626ea8
HARINIM2405 <149925922+HARINIM2405@users.noreply.github.com>
2025-01-07 10:08:03 +0530

    change made

--- src/User.jsx
+++ src/User.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import "./user.css";
+import "./User.css";
 
 const User = () => {
   const [users, setUsers] = useState([]);
